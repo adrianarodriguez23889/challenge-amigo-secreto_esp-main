908a9799925188f72061338e19aefbe187fa30d3
Adriana Rodriguez <bettyberodriguez@gmail.com>
2025-08-01 11:40:09 -0300

    Primera función que valida y agrega nombres al array

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo(){
+    //se captura el valor del imput
+    let input = document.getElementById("amigo");
+    //se eliminan los espacios en blanco
+    let nombre = Input.value.trim();
+
+    //vemos que no esté vacío
+    if(nombre === ""){
+        alert("Por favor, incerte un nombre.");
+        return;
+    }
+    //agregando un nombre en el array
+    amigos.push(nombre);
+
+    //limpiando el campo
+    input.value = "";
+}
